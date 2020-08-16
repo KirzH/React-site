c253321fe0e82f609399b4d57c9abd768a9c1e66
KirzH <kirzhakov.2012@mail.ru>
2020-08-16 15:49:03 +0300

    fix

---
+++ src/components/CarouselBox.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import waterImg from '../assets/water.jpg';
+import tigerImg from '../assets/tiger.jpg';
+
+export default class CarouselBox extends Component {
+    render() {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={waterImg}
+                        alt="Water"
+                    />
+                    <Carousel.Caption>
+                        <h3>Water Images</h3>
+                        <p>Drinking water is a clear liquid without taste, 
+                        color and smell, which is intended for safe consumption, cooking and drinks.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={tigerImg}
+                        alt="Tiger"
+                    />
+                    <Carousel.Caption>
+                        <h3>Tiger Images</h3>
+                        <p>The tiger is the largest wild cat with a massive, muscular flexible body.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        )
+    }
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import { Navbar, Container, Form, FormControl, Nav, Button } from 'react-bootstrap'
-
 import logo from './logo192.png'
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Switch from 'react-bootstrap/esm/Switch'
 
+import Home from '../pages/Home';
+import About from '../pages/About'
+import Contacts from '../pages/Contacts';
+import Blog from '../pages/Blog';
 
 export default class Header extends Component {
     render() {
@@ -18,14 +22,14 @@ export default class Header extends Component {
                                 width="30"
                                 className="d-inline-block align-top"
                                 alt="Logo"
-                            />
+                            /> React site
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="respo0nsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav" >
                             <Nav className="mr-auto">
                                 <Nav.Link href="/"> Home </Nav.Link>
                                 <Nav.Link href="/about"> About us </Nav.Link>
-                                <Nav.Link href="/contacs"> Contacs </Nav.Link>
+                                <Nav.Link href="/contacts"> Contacts </Nav.Link>
                                 <Nav.Link href="/blog"> Blog </Nav.Link>
                             </Nav>
                             <Form inline>
@@ -39,7 +43,16 @@ export default class Header extends Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/contacts" component={Contacts} />
+                        <Route exact path="/blog" component={Blog} />
+                    </Switch>
+                </Router>
             </>
-        )
+        );
     }
 }
