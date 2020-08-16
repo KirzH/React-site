5b7dda734122886109e37507b155e84a448afdf8
KirzH <kirzhakov.2012@mail.ru>
2020-08-16 18:08:09 +0300

    fix-about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import  { Container, Tab, Nav, Row, Col } from 'react-bootstrap';
+
+
 
 export default class About extends Component {
     render() {
         return (
             <Container>
-            
+                <Tab.Container id="lebt-tabs-example" defaultActiveKey="first" >
+                    <Row>
+                        <Col sn={3}>
+                            <Nav variant="pills" className="flex-column mt-2" >
+                                <Nav.Item>
+                                    <Nav.Link eventkey="first">Design</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventkey="second">Team</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventkey="third">Programing</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventkey="fourth">Frameworks</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventkey="fifth">Libraries</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventkey="first">
+                                    <img src="https://i.ytimg.com/vi/Kf1ggrgkrjgILChfks/maxresdefault.jpg" />
+                                    <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventkey="second">
+                                    <img src="https://i.ytimg.com/vi/Kf1gILChfks/maxresdefault.jpg" />
+                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventkey="third">
+                                    <img src="https://i.ytimg.com/vi/Kf1gILChfks/maxresdefault.jpg" />
+                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventkey="foutth">
+                                    <img src="https://i.ytimg.com/vi/Kf1gILChfks/maxresdefault.jpg" />
+                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventkey="fifth">
+                                    <img src="https://i.ytimg.com/vi/Kf1gILChfks/maxresdefault.jpg" />
+                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
             </Container>
         )
     }
